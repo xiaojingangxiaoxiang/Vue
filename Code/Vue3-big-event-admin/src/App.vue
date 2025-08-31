b8c3b73aa30beeacc9d65feef2dc91a8e7b30f2d
YMSS <1341424760@qq.com>
2025-08-31 21:47:57 +0800

    Day4

---
+++ Code/Vue3-big-event-admin/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// 错误
+// const goList= () =>{
+//   this.$router.push('/list')
+// }
+// 在Vue3中，setup语法糖中没有this
+import { useRoute , useRouter } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+const goList = () =>{
+  console.log(route, router)
+  router.push('/list')
+}
+</script>
+
+<template>
+  <div>我是App
+    <button @click="$router.push('/home')">跳首页</button>
+    <button @click="goList">跳列表页</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
